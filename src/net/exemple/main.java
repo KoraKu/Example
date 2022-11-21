0437201df73269d3f8d751c03c0436ec79c7e9a7
KoraKu <costhug@orange.fr>
2022-11-21 10:26:09 +0100

    4th commit

--- src/net/exemple/main.java
+++ src/net/exemple/main.java
@@ -6,6 +6,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello Software Engineering");
 
-        String password = "apQ48z%m";
+        String password = "apQ48z%m58j58s6g894g6f*g+dh+-5n8;7s5g254q8t7s5545EfdUsjjjZ58dc55q7fScg";
     }
 }
