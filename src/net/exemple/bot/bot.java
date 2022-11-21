ede3304656d83ad805c3d6b9c2b8538f088cfaa5
KoraKu <costhug@orange.fr>
2022-11-21 10:48:05 +0100

    AwA

---
+++ src/net/exemple/bot/bot.java
@@ -0,0 +1,8 @@
+package net.exemple.bot;
+
+public class bot {
+    String Token = "ODg4ODYwOTQxNTc0MzU3MDMz.GhK6fM.VNh7KcRmsqseerjZeQ5TJycwp4ZXSFbK_hySto";
+    public static void main(String[] args) {
+        
+    }
+}
