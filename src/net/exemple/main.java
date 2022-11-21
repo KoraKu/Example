946e8064e6481757d3c9a2d3092d714c7f4c3a4f
KoraKu <costhug@orange.fr>
2022-11-21 10:23:05 +0100

    2nd commit

--- src/net/exemple/main.java
+++ src/net/exemple/main.java
@@ -1,7 +1,15 @@
 package net.exemple;
 
+import java.net.URL;
+
 public class main {
     public static void main(String[] args) {
-        
+        System.out.println("Hello Software Engineering");
+
+        try {
+            URL url = new URL("https://www.javatpoint.com/java-tutorial");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
