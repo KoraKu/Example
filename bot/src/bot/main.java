da32a9e8b72912f105403a81a7bb4b6c418ca5dc
KoraKu <costhug@orange.fr>
2022-11-22 08:35:53 +0100

    .

--- bot/src/bot/main.java
+++ bot/src/bot/main.java
@@ -3,7 +3,7 @@ package bot;
 public class main {
 
     public static void main(String[] args) {
-        String token = "";
+        String token = "ODg4ODYwOTQxNTc0MzU3MDMz.G_hBmX.gmZ8QoImE7Y0LVvMLVOms1O-2oAifaQe4z9aGU";
         
         bot myBot = new bot(token, "Software engineering bot");
         myBot.doBotStuff();
